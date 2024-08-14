1feef1e4979e31b1b000e8456fa4801945568d10
Nick Kataev <goodfella9091@gmail.com>
2024-08-14 23:42:38 +0300

    HorseTest done at all

--- src/test/java/HorseTest.java
+++ src/test/java/HorseTest.java
@@ -3,7 +3,10 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 
+import java.nio.charset.Charset;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -27,7 +30,7 @@ public class HorseTest {
     }
     @ParameterizedTest
     @MethodSource("SpaceCharactersForHorseNameString")
-    public void whenEmptyOrSpacesNameString() {
+     void whenEmptyOrSpacesNameString() {
         Throwable exception = assertThrows(
                 IllegalArgumentException.class,
                 () -> {
@@ -59,17 +62,19 @@ public class HorseTest {
         );
         assertEquals("Distance cannot be negative.", exception.getMessage());
     }
-
+    @Test
     public void whenGetNameReturns()
     {
         Horse horse = new Horse("Simon",10);
         assertEquals("Simon",horse.getName());
     }
+    @Test
     public void whenGetSpeedReturns()
     {
         Horse horse = new Horse("Simon",20);
         assertEquals(2,horse.getSpeed());
     }
+    @Test
     public void whenGetDistanceReturns()
     {
         Horse horse = new Horse("Simon",30,50);
@@ -77,4 +82,27 @@ public class HorseTest {
         Horse horseWithoutDistance = new Horse("Simon",30);
         assertEquals(0,horse.getDistance());
     }
+    @Test
+    public void checkRandomDouble()
+    {
+        try (MockedStatic<Horse> horseMockedStatic =  Mockito.mockStatic(Horse.class)) {
+            Horse horse = new Horse("Pyoes", 20, 30);
+            horse.move();
+            horseMockedStatic.verify(() -> Horse.getRandomDouble(0.2, 0.9));
+
+        }
+    }
+    @ParameterizedTest
+    @ValueSource(doubles = {1, 2})
+    void method_move_check_getRandomDouble_sum(double numbers) {
+        try (MockedStatic<Horse> horseMockedStatic = Mockito.mockStatic(Horse.class)) {
+            Horse horse = new Horse("Pyoes", 20, 30);
+            horseMockedStatic.when(() -> Horse.getRandomDouble(0.2, 0.9)).thenReturn(numbers);
+            double distance = horse.getDistance();
+            horse.move();
+            assertEquals(distance + horse.getSpeed() * numbers, horse.getDistance());
+        }
+    }
 }
+
+
