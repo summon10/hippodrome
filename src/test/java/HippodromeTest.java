521207bad564fd9658fecec7f3b1706b321be400
Nick Kataev <goodfella9091@gmail.com>
2024-08-15 23:08:29 +0300

    HorseTest done at all Hippodrome done at all

--- src/test/java/HippodromeTest.java
+++ src/test/java/HippodromeTest.java
@@ -1,11 +1,19 @@
 import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.exceptions.base.MockitoException;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
+import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 public class HippodromeTest {
+    @Mock
+    List<Horse> mockHorsesList;
 @Test
 public void whenNullHorses() {
     Throwable exception = assertThrows(
@@ -27,5 +35,35 @@ public void whenEmptyOrSpacesString() {
         );
         assertEquals("Horses cannot be null.", exception.getMessage());
     }
+@Test
+     public void getHorsesTest()
+     {
+         List<Horse> horseList = new ArrayList<>();
+         for (int i = 0; i < 30; i++) {
+             horseList.add(new Horse("Name"+i, i));
 
+         }
+        Hippodrome hippodrome = new Hippodrome(horseList);
+         assertArrayEquals(horseList.toArray(), hippodrome.getHorses().toArray(new Horse[0]));
+     }
+     @Test
+    public void moveTest() {
+         for (int i = 0; i < 50; i++) {
+             mockHorsesList.add(new Horse("name" + i, i));
+         }
+         Hippodrome hippodrome = new Hippodrome(mockHorsesList);
+         hippodrome.move();
+         for (Horse horse : mockHorsesList) {
+             Mockito.verify(horse).move();
+         }
+     }
+    @Test
+   public void getWinner() {
+        List<Horse> horses = new ArrayList<>();
+        for (int i = 0; i < 50; i++) {
+            horses.add(new Horse("Name", 1, i));
+        }
+     Hippodrome hippodrome = new Hippodrome(horses);
+        assertEquals(49, hippodrome.getWinner().getDistance());
+    }
 }
